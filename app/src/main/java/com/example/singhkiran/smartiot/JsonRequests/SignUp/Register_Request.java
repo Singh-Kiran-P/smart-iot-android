bf2e7c555f15fac0a475c4389e2115da77aca0c3
Singh Kiran <singh.kiran2456@hotmail.com>
2019-01-21 00:49:22 +0100

    register working

--- app/src/main/java/com/example/singhkiran/smartiot/JsonRequests/SignUp/Register_Request.java
+++ app/src/main/java/com/example/singhkiran/smartiot/JsonRequests/SignUp/Register_Request.java
@@ -9,9 +9,12 @@ import com.example.singhkiran.smartiot.JsonRequests.API_Info.API_Server;
 import com.example.singhkiran.smartiot.JsonRequests.SmartiotAPI;
 import com.example.singhkiran.smartiot.JsonRequests.retrofit2_config;
 import com.example.singhkiran.smartiot.R;
+import com.example.singhkiran.smartiot.UI.LoginActivity;
 import com.example.singhkiran.smartiot.UI.SignupActivity;
 
 import java.io.IOError;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,33 +22,51 @@ import retrofit2.Callback;
 public class Register_Request {
     private SmartiotAPI smartiotAPI;
     Context context;
-    //get value's from inputbox login and password
-    final EditText name = ((SignupActivity) context).findViewById(R.id.et_name);
-    final EditText email = ((SignupActivity) context).findViewById(R.id.et_email);
-    final EditText username = ((SignupActivity) context).findViewById(R.id.et_username);
-    final EditText password = ((SignupActivity) context).findViewById(R.id.et_password);
-    final EditText password2 = ((SignupActivity) context).findViewById(R.id.et_password2);
+    String Name;
+    String Email;
+    String Username;
+    String Password;
+
 
     public void MakeRequest(final Context context1) {
         context = context1;
+        //get value's from inputbox login and password
+        EditText name = ((SignupActivity) context).findViewById(R.id.et_username);
+        EditText email = ((SignupActivity) context).findViewById(R.id.et_email);
+        EditText username = ((SignupActivity) context).findViewById(R.id.et_username);
+        EditText password = ((SignupActivity) context).findViewById(R.id.et_password);
+        EditText password2 = ((SignupActivity) context).findViewById(R.id.et_password2);
 
-        API_Server api_server = new API_Server();
-        if (password != password2) {
-            Toast.makeText(context, "Password do not match", Toast.LENGTH_SHORT).show();
+        Name = name.getText().toString();
+        Email = email.getText().toString();
+        Username = username.getText().toString();
+        Password = password.getText().toString();
+
+
+        Pattern p = Pattern.compile(".+@.+\\.[a-z]+");
+        Matcher m = p.matcher(Email);
+        boolean matchFound = m.matches();
+        if (matchFound) {
+            if (!password.getText().toString().equals(password2.getText().toString())) {
+                Toast.makeText(context, "Password do not match", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            retrofit2_config retrofit2 = new retrofit2_config();
+            smartiotAPI = retrofit2.retrofit.create(SmartiotAPI.class);
+            createRequest();
+        }if(!matchFound) {
+            Toast.makeText(context, "Invaild Email", Toast.LENGTH_SHORT).show();
+
         }
 
-        retrofit2_config retrofit2 = new retrofit2_config();
-        smartiotAPI =retrofit2.retrofit.create(SmartiotAPI.class);
-        createRequest();
+
 
     }
 
     private void createRequest() {
-        String Name = name.getText().toString();
-        String Email = email.getText().toString();
-        String Username = username.getText().toString();
-        String Password = password.getText().toString();
-        final Register_Model register = new Register_Model(Name,Email,Username,Password);
+
+        Register_Model register = new Register_Model(Name, Email, Username, Password);
 
 
         Call<Register_Model> call = smartiotAPI.registerPost(register);
